d359405793f5564beaf41b03ed485d2307d3157e
fgsreally <1325041831@qq.com>
2023-05-13 10:32:32 +0800

    fix:fix vite plugin types declare

--- src/vite.ts
+++ src/vite.ts
@@ -1,4 +1,17 @@
 import type { VitePlugin } from 'unplugin'
+import type { MetaCheckerOptions } from 'vue-component-meta'
 import { sfcmore } from './plugin'
 import type { Extension } from './type'
-export const sfc = sfcmore.vite as (options?: { version?: string; extensions?: Extension[] }) => VitePlugin
+
+export const sfc = sfcmore.vite as (options?: {
+  /** 是否生成vue的meta */
+  meta?: boolean
+  version?: string
+  extensions?: Extension[]
+  /** 是否加入源码 */
+  copysource?: boolean
+  checkerOptions?: MetaCheckerOptions
+  /** 是否输出产物 */
+
+  write?: boolean
+}) => VitePlugin
